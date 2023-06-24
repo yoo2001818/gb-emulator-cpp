9e87231351dbc08f9cfc03b03851e91de36d8763
Duknam Yoo <yoo2001818@gmail.com>
2023-06-24 23:58:12 +0900

    Add node element definition

--- src/ui/node.hpp
+++ src/ui/node.hpp
@@ -1,13 +1,43 @@
+#include <memory>
+#include <vector>
+
 #ifndef __NODE_HPP__
 #define __NODE_HPP__
 
 namespace ui
 {
+  class render_handle;
   class node
   {
   public:
     node();
     virtual ~node();
+    virtual void layout() = 0;
+    virtual void render(render_handle &mRenderHandle) = 0;
+    int width();
+    void width(int mWidth);
+    int height();
+    void height(int mHeight);
+
+  private:
+    std::weak_ptr<node> mParent;
+    int mWidth;
+    int mHeight;
+  };
+  class element : public node
+  {
+  public:
+    element();
+    virtual ~element();
+    virtual void layout() = 0;
+    virtual void render(render_handle &mRenderHandle) = 0;
+
+    void addChild(std::shared_ptr<node> pNode);
+    void removeChild(std::shared_ptr<node> pNode);
+    std::vector<std::shared_ptr<node>> &children();
+
+  private:
+    std::vector<std::shared_ptr<node>> mChildren;
   };
 }
 
